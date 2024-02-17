08da18c3ba3803b0ff938bb01ee996b0c67f65c4
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-17 22:09:34 +0530

    Anatomy of the Command Line interface

---
+++ 01-anatomy of the command line interface.js
@@ -0,0 +1,43 @@
+// ========== Command Line ==========// 
+
+// pwd
+
+// pwd
+
+// pwd--help
+
+// clear
+
+// // ------------------------
+
+// cd / home
+
+// pwd
+
+// cd / home / akram
+
+// cd..
+// pwd
+
+// cd / home / akram
+
+// cd../..
+// pwd
+
+// pwd
+
+// pwd 
+
+// cd home
+
+// cd akram
+
+// cd..
+
+// cd../ akram
+
+// cd../ akram /../ akram
+
+
+
+
