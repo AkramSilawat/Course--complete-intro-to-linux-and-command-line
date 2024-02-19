7f05a0dbb8e1e30386a297bb712a30a456b4939a
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-19 19:22:10 +0530

    The CLI

---
+++ 01-the cli.js
@@ -0,0 +1,216 @@
+// ========== Anatomy of the Command ==========// 
+
+// pwd
+
+// pwd
+
+// pwd--help
+
+// clear
+
+// // ------------------------
+
+// cd / home
+
+// pwd
+
+// cd / home / akram
+
+// cd..
+
+// pwd
+
+// cd / home / akram
+
+// cd../..
+
+// pwd
+
+// pwd
+
+// pwd 
+
+// cd home
+
+// cd akram
+
+// cd..
+
+// cd../ akram
+
+// cd../ akram /../ akram
+
+
+
+
+
+// ========== CLI Directories & Arguments ========== //
+
+// ls
+
+// cd .. 
+// pwd
+
+// ls
+
+
+// pwd
+
+// ls home
+
+// ls --help
+
+// ---- Usage: ls [OPTION]... [FILE]...
+// 
+// clear
+
+// ----------------------------------------------------
+
+// echo hi
+
+// echo welcome to frontend masters
+
+// echo "welcome to frontend masters"
+
+
+
+// which ls
+
+// cd /bin   // ------ bin directories
+
+// ls
+
+
+// cd /home
+
+// pwd
+
+// clear 
+
+
+
+
+// ========== Flags ========== //
+
+// pwd --help
+
+// clear
+
+// ---------
+
+// echo --help
+
+// man echo
+
+// echo --help
+
+// clear
+
+// ---------------
+
+// /home$ ls 
+
+// /home$ ls -l
+
+// /home$ ls -l
+
+// /home$ ls -l -a
+
+// /home$ touch .hidden-file
+
+// /home$ cd akram/
+
+// /home$ touch .hidden-file
+
+// /home$ ls -l -a
+
+// /home$ ls -l
+
+// /home$ clear
+
+// -------------------------
+
+// /home$ ls -h
+
+// /home$ ls -- help
+
+// /home$ ls -- all
+
+// /home$ ls -- all -a
+
+// /home$ ls -a
+
+// /home$ ls -l -a
+
+// /home$ ls -al
+
+// /home$ ls -la
+
+// /home$ ls --all -l
+
+// clear
+
+// -----------------------
+
+// :~$ ls
+
+// :~$ ls --ignore=snap
+
+// :~$ ls --ignore snape
+
+// :~$ ls --ignore snap
+
+// :~$ ls --ignore=snap --ignore=Home 
+
+// :~$ clear
+
+// ----------------------------
+
+// :~$ ls -lsah
+
+// :~$ ls --help
+
+// :~$ clear 
+
+// ----------------------------
+
+
+// :~$ ls --ignore snap
+
+// :~$ ls -I snap
+
+// :~$ ls --help 
+
+// :~$ clear
+
+// ----------------------------
+
+
+// :~$ pwd 
+
+// :~$ ls snap/
+
+// :~$ ls -lsah snap
+
+// :~$ ls snap -lash 
+
+// :~$ ls -lash snap
+
+// :~$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
