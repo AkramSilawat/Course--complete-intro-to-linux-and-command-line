048159ed93cbebdb713d669fbf6f33a992cc49ca
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-21 22:31:08 +0530

    Editors

---
+++ 02-editors.js
@@ -0,0 +1,16 @@
+ // ============ Nano ============ //
+ 
+ // :~$  kill -l
+
+ // :~$  cd ~
+
+  // :~$  nano textfile.txt    -----> GUN nano 2.9.3      textfile.txt
+
+
+
+
+// -- this is a textt file
+
+// -- I can write whatever I want
+
+// -- this is so cool
