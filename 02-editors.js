400a90c757b63a74bbb17b0f2be4917a5046ceb2
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-23 12:33:36 +0530

    Editors

--- 02-editors.js
+++ 02-editors.js
@@ -13,3 +13,48 @@
 
 // -- this is so cool 
 
+
+
+// =========== VIM: A Brief History ============ //
+
+ // :~$  ed ed.txt
+
+ /**
+  * - output: ed.text: NO such file or directory
+  * - sdfklj
+  * - ?
+  * - lk
+  * - ?
+  * - ^C
+  * - ?
+  * 
+  * -> press CTL+D exit 
+  * 
+  */
+
+ // :~$  ls
+
+ // :~$  ls -lsah
+
+
+// :~$  ex textfile.txt
+
+// -->> " textfile.txt" 8L, 88C
+// -->> Entering Ex mode. Type "visual" to go to Normal mode.
+// :visual
+
+// E501: At end-of-file
+
+
+// :~$  vi textfile.txt
+
+// :~$  clear
+
+
+
+
+
+
+
+
+
