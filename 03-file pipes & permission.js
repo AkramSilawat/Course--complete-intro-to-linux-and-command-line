3d605caa69d94615c3bb5869c18ec8978c9d4e45
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-02-25 22:09:53 +0530

    Fill Popes & Permission

---
+++ 03-file pipes & permission.js
@@ -0,0 +1,16 @@
+// ============ Reading Files ============ //
+
+// ---> LESS <---
+
+// :~$ less textfile.txt
+
+// :~$ ls 
+// - textfile.txt  test1.c   test2.c   test3.cpp
+
+// :~$  vi textfile.txt
+
+// :~$ less 
+
+// :~$ less textfile.txt
+
+// :~$ more textfile.txt
