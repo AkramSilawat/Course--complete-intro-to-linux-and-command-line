c865cd0ce3e2c677144689403827d6ac3b072740
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-02 21:50:58 +0530

    En

---
+++ 04-environments.js
@@ -0,0 +1,26 @@
+ // ============ Environment ============ //
+
+// :~$  printenv               // ---> long list of environmental variables.                             
+
+// :~$  echo $USER
+
+// :~$  USER=brian            // ---> set the value of an environment variable.
+
+// :~$  echo Hello My Name is  $USER             // ---> prints "Hello My Name is brian"
+
+// :~$  echo $GREETING $USER, How are you ?      // ---> prints "Hello My Name is brian, How are you ?"
+
+// :~$  GREETING="Hello"         
+
+// :~$  echo $GREETING $USER, How are you ?      // ---> prints "Hello Brian, How are
+
+// :~$  sudo vi /etc/environment 
+// ---> Exit vim :  type ':qa!'[ENTER]
+
+// :~$  echo $TEST_THINK
+
+// :~$  sudo vi /etc/environment 
+
+// :~$  exit
+
+
