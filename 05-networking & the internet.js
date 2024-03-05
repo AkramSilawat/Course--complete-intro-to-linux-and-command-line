700614ede4cbd641ee09b6931252a54e25aa04de
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-05 13:15:55 +0530

    Networking & the internet

---
+++ 05-networking & the internet.js
@@ -0,0 +1,24 @@
+// ============ SSH & Secondary Machine ============ //
+
+// :~$  multipass launch --name secondary    ---->>  start a second Linux VM named secondary.
+
+// :~$  multipass shell secondary            ---->>  an example of multipass launch name secondary
+
+// ------------------------------
+
+// :~$  sudo useradd -s /bin/bash -m -g ubuntu brian        ---->> create a new user called "brian
+
+// :~$  sudo passwd brian           ---->> add the user "brian" and give it access to bash, make home directory
+
+// :-$  su brian
+
+// :/home/akram$  ls
+
+// :/home/akram$  cd ..
+
+// :/home$  ls
+// --->> brian  akram
+
+
+
+
