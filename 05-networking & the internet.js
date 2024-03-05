2af5772e1d38bbd855b4df7df3bbb67485a1f779
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-05 15:07:25 +0530

    networking & the internet

--- 05-networking & the internet.js
+++ 05-networking & the internet.js
@@ -22,3 +22,28 @@
 
 
 
+// ============ Linking two Machines with SSH ============ //
+
+// :~$ ssh-keygen -t rsa          ---->> generate the RSA type key pair
+
+// - Generating public/private rsa key pair.
+// - Enter file in which to save the key (/home/akram/.ssh/id_rsa):
+// - Enter passphrase (empty for no passphrase):
+// - Enter same passphrase again:
+/* * Your identification has been saved in /home/akram/.ssh/id_rsa
+   * Your public key has been saved in /home/akram/.ssh/id_rsa.pub
+   * The key fingerprint is:
+   * SHA256:y3LSueAK734vO19rvdHfLS8+m5l5kOCPQH/PwJtvnPg dell@DESKTOP-751INM5
+   * The key's randomart image is:
+   *  +---[RSA 3072]----+
+      |                 |
+      |                 |
+      |                 |
+      |          . .    |
+      |        S. o o . |
+      |       o o. o.*  |
+      |  .   + * .o.+oO.|
+      |   o .+= o..ooBBO|
+      |   o=oo*+.. .oOE*| 
+      +----[SHA256]-----+
+    */                                
