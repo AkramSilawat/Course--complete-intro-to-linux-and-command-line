f2106fe79d8ea722df5fc608c1273f35fd1088dd
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-08 22:41:36 +0530

    Pacakage management

---
+++ 06-package management.js
@@ -0,0 +1,31 @@
+// ========== Package Managment & APT Basics ========== //
+
+// :~$  dpkg --help
+// --->>  Usage: dpkg [options] command [arguments...]
+
+// :~$  sudo apt install aptitud            // --->> Installing aptitude tool   
+
+
+// :~$  aptitude                
+
+// :~$  apt-get install   aptitude          // --->> Installing aptitude using apt       
+
+// :~$  apt install lolcat              colorfull word
+
+// :~$  lolcat --help                       // --->> Checking if the package is installed correctly or not. 
+
+// :~$  cat log.txt 
+
+// :~$  lolcat log.txt   
+
+//-----------------------------
+
+// :~$  sudo apt install nodejs        // --->> install
+
+// :~$  node  -v           // --->> Check Node Version
+
+// :~$  node -e "console.log('hello from node')"     // ---->> hello from node
+
+// :~$  apt search nodejs                            // ---->> Search for packages related to nodejs
+
+// :~$  sudo apt remove nodejs
