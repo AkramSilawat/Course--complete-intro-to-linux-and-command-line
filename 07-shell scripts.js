77292c64d854b0c37e8dab5ba58ddbf98d0ee7ad
AkramSilawat <akram.khan.silawat.aks@gmail.com>
2024-03-09 12:37:39 +0530

    Shell Scripts

--- 07-shell scripts.js
+++ 07-shell scripts.js
@@ -48,8 +48,83 @@
 
       */
 
-// :~$  chmod +x gen_files                    
+// :~$  mv gen_files.sh gen_files
+
+// :~$  ls
+
+// :~$  chmod +x gen_files  
+
+// :~$  ls
 
 // :~$  ls -lsah
 
-// :~$  ./genfile        ----->> done
+// :~$  ./gen_files      
+// --->> done
+
+
+// ------------------------------------------
+
+
+// :~$  which node              
+// --->> /usr/local/bin/node
+
+// :~$  vi node_test
+   /*
+      - *#! /snap/bin/node
+      - console.log('this is running from node.js')
+        */
+
+// :~$  chmod +x node_test
+
+// :~$  ./node_test             
+// --->> this is running from node.js
+
+
+
+
+// ============> Path & Variable <============ //
+
+// :~$  echo $PATH
+// -->> /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin
+
+// :~$  ls /user/local/sbin
+// :~$  ls /user/local/games
+// :~$  ls /snap/bin
+/* 
+   - code              firefox.geckodriver  snap-store.ubuntu-software
+   - code.url-handler  geckodriver          snap-store.ubuntu-software-local-file
+   - firefox           snap-store
+   */
+
+// :~$  mkdir my_bin
+
+// :~$  m node_test my_bin
+
+// :~$  mv gen_file my_bin
+
+// :~$  cd my_bin/
+
+// :~/my_bin$  ls
+// -->> gen_file   node_test
+
+// :~/my_bin$  cd ..
+
+// :~$  vi .bashrc   
+// --->> Alias definitons.
+// --->> You may want to put all your additons into a separate file like
+
+// --->> # added by me for testing purpose
+// --->> PATH=$PATH:/home/mohit/my_bin
+
+// :wq
+
+// // :~$  clear
+
+// ---------------------------------
+
+// :~$  node_test
+// --> this is running from node.js
+
+// :~$  gen_files
+// --> done
+
